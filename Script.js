de374ef7b91f9c74045d57d49fc358e7623cc57d
Kishore-Bollam <105742707+Kishore-Bollam@users.noreply.github.com>
2023-07-06 13:22:25 +0530

    HTML Registration and login form

---
+++ Script.js
@@ -0,0 +1,8 @@
+
+
+let listContainer = document.getElementById('list');
+let listElement = document.createElement('ul');
+let item = document.createElement("li");
+item.innerText="webla"
+listElement.appendChild(item);
+listContainer.appendChild(listContainer);
